0c59c98fd75e02667ad6de6294c27150f5d1d0f1
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-13 12:40:09 +1030

    styling added

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+const add = function(num1, num2){
+    return num1 + num2;
+}
+
+const subtract = function(num1, num2){
+    return num1 - num2;
+}
+
+const multiply = function(num1, num2){
+    return num1 * num2;
+}
+
+const divide = function(num1, num2){
+    return num1 / num2
+}
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = ""
+
 const label = document.querySelector("#display")
 const numberButtons = document.querySelectorAll(".number")
 
 numberButtons.forEach((button) => {
-    button.addEventListener("click", label.textContent+= button.textContent)
+    button.addEventListener("click", () => {
+        label.textContent += button.textContent;
+    })
 })
+
+
