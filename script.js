7bab5713994dd43c2c789b815dca1cbd8a3197f9
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-13 13:10:18 +1030

    event listeners added to numbers and decimal

--- script.js
+++ script.js
@@ -11,20 +11,26 @@ const multiply = function(num1, num2){
 }
 
 const divide = function(num1, num2){
-    return num1 / num2
+    return num1 / num2;
 }
 
 let firstNumber = 0;
 let secondNumber = 0;
-let operator = ""
+let operator = "";
 
-const label = document.querySelector("#display")
-const numberButtons = document.querySelectorAll(".number")
+const label = document.querySelector("#display");
+const numberButtons = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const decimal = document.querySelector("#decimal");
 
 numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
         label.textContent += button.textContent;
     })
-})
+});
 
-
+decimal.addEventListener("click", () => {
+    if (!label.contains(".")) {
+        label.textContent += button.textContent;
+    }
+});
